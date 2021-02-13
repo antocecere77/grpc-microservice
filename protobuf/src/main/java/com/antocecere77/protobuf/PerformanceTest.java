92ad8f14da8678500f03ad93b71e7e1888aaa49f
antocecere <antocecere77@gmail.com>
2021-02-13 11:20:41 +0100

    proto vs json - performance comparison - part - 2

--- protobuf/src/main/java/com/antocecere77/protobuf/PerformanceTest.java
+++ protobuf/src/main/java/com/antocecere77/protobuf/PerformanceTest.java
@@ -1,14 +1,46 @@
 package com.antocecere77.protobuf;
 
+import com.antocecere77.json.JPerson;
+import com.antocecere77.models.Person;
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.google.protobuf.InvalidProtocolBufferException;
+
+import java.io.IOException;
+
 public class PerformanceTest {
 
     public static void main(String[] args) {
 
+        JPerson person = new JPerson();
+        person.setName("sam");
+        person.setAge(10);
+        ObjectMapper mapper = new ObjectMapper();
+
         //json
-        Runnable runnable1;
+        Runnable runnable1 = () -> {
+            try {
+                byte[] bytes = mapper.writeValueAsBytes(person);
+                JPerson person1 = mapper.readValue(bytes, JPerson.class);
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
+        };
 
         //protobuf
-        Runnable runnable2;
+        Person sam = Person.newBuilder()
+                .setName("sam")
+                .setAge(10)
+                .build();
+
+        Runnable runnable2 = () -> {
+            try {
+                byte[] bytes = sam.toByteArray();
+                Person sam1 = Person.parseFrom(bytes);
+            } catch (InvalidProtocolBufferException e) {
+                e.printStackTrace();
+            }
+        };
     }
 
     public static void runPerformanceTest(Runnable runnable, String method) {
