fbe2eb9eda0bdec3c852738e5bcbf798d885add0
antocecere <antocecere77@gmail.com>
2021-02-13 11:02:53 +0100

    proto - equals method

--- protobuf/src/main/java/com/antocecere77/protobuf/PersonDemo.java
+++ protobuf/src/main/java/com/antocecere77/protobuf/PersonDemo.java
@@ -5,11 +5,18 @@ import com.antocecere77.models.Person;
 public class PersonDemo {
 
     public static void main(String[] args) {
-        Person sam = Person.newBuilder()
+        Person sam1 = Person.newBuilder()
                 .setName("sam")
                 .setAge(18)
                 .build();
 
-        System.out.println(sam);
+        Person sam2 = Person.newBuilder()
+                .setName("sam")
+                .setAge(18)
+                .build();
+
+        System.out.println(sam1);
+
+        System.out.println(sam1.equals(sam2));
     }
 }
