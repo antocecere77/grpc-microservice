7bebf7ffe004e3d6a1f24a93639c18a963ddcaba
antocecere <antocecere77@gmail.com>
2021-02-15 06:29:58 +0100

    proto - collection demo

--- protobuf/src/main/java/com/antocecere77/protobuf/CompositionDemo.java
+++ protobuf/src/main/java/com/antocecere77/protobuf/CompositionDemo.java
@@ -4,6 +4,9 @@ import com.antocecere77.models.Address;
 import com.antocecere77.models.Car;
 import com.antocecere77.models.Person;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class CompositionDemo {
 
     public static void main(String[] args) {
@@ -14,17 +17,29 @@ public class CompositionDemo {
                 .setCity("Atlanta")
                 .build();
 
-        Car car = Car.newBuilder()
+        Car accord = Car.newBuilder()
                 .setMake("Honda")
                 .setModel("Accord")
                 .setYear(2020)
                 .build();
 
+        Car civic = Car.newBuilder()
+                .setMake("Honda")
+                .setModel("Civic")
+                .setYear(2005)
+                .build();
+
+        List<Car> cars = new ArrayList<>();
+        cars.add(civic);
+        cars.add(accord);
+
         Person sam = Person.newBuilder()
                 .setName("sam")
                 .setAge(25)
                 .setAddress(address)
-                .setCar(car)
+                //.addCar(accord)
+                //.addCar(civic)
+                .addAllCar(cars)
                 .build();
 
         System.out.println(sam);
