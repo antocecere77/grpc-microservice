f953bfc400a34d140c3781de61eb8340607caad6
antocecere <antocecere77@gmail.com>
2021-02-15 07:21:35 +0100

    proto - oneof - demo

---
+++ protobuf/src/main/java/com/antocecere77/protobuf/OneOfDemo.java
@@ -0,0 +1,40 @@
+package com.antocecere77.protobuf;
+
+import com.antocecere77.models.Credentials;
+import com.antocecere77.models.EmailCredentials;
+import com.antocecere77.models.PhoneOTP;
+
+public class OneOfDemo {
+
+    public static void main(String[] args) {
+
+        EmailCredentials emailCredentials = EmailCredentials.newBuilder()
+                .setEmail("nobody@gmail.com")
+                .setPassword("admin123")
+                .build();
+
+        PhoneOTP phoneOTP = PhoneOTP.newBuilder()
+                .setNumber(12345678)
+                .setCode(456)
+                .build();
+
+        Credentials credentials = Credentials.newBuilder()
+                .setEmailMode(emailCredentials)
+                .setPhoneMode(phoneOTP)
+                .build();
+
+        login(credentials);
+    }
+
+    private static void login(Credentials credentials) {
+
+        switch (credentials.getModeCase()) {
+            case EMAILMODE:
+                System.out.println(credentials.getEmailMode());
+                break;
+            case PHONEMODE:
+                System.out.println(credentials.getPhoneMode());
+                break;
+        }
+    }
+}
