fbe1daf7f2deed2d347ecd91a124c8d143f740e7
antocecere <antocecere77@gmail.com>
2021-02-15 06:35:11 +0100

    proto - map demo

---
+++ protobuf/src/main/java/com/antocecere77/protobuf/MapDemo.java
@@ -0,0 +1,31 @@
+package com.antocecere77.protobuf;
+
+import com.antocecere77.models.Car;
+import com.antocecere77.models.Dealer;
+
+public class MapDemo {
+
+    public static void main(String[] args) {
+        Car accord = Car.newBuilder()
+                .setMake("Honda")
+                .setModel("Accord")
+                .setYear(2020)
+                .build();
+
+        Car civic = Car.newBuilder()
+                .setMake("Honda")
+                .setModel("Civic")
+                .setYear(2005)
+                .build();
+
+        Dealer dealer = Dealer.newBuilder()
+                .putModel(2005, civic)
+                .putModel(2020, accord)
+                .build();
+
+        System.out.println(dealer.getModelOrThrow(2005));
+        System.out.println(dealer.getModelOrDefault(2019, accord));
+
+        System.out.println(dealer.getModelMap());
+    }
+}
