76fa1b68c2342e9409e52fab068e336f9fe7f17a
antocecere <antocecere77@gmail.com>
2021-02-17 06:39:42 +0100

    unary - client's blocking stub

---
+++ grpc-intro/src/test/java/com/antocecere77/client/BankClientTest.java
@@ -0,0 +1,36 @@
+package com.antocecere77.client;
+
+import com.antocecere77.models.Balance;
+import com.antocecere77.models.BalanceCheckRequest;
+import com.antocecere77.models.BankServiceGrpc;
+import com.antocecere77.server.BankService;
+import io.grpc.ManagedChannel;
+import io.grpc.ManagedChannelBuilder;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.TestInstance;
+
+@TestInstance(TestInstance.Lifecycle.PER_CLASS)
+public class BankClientTest {
+
+    private BankServiceGrpc.BankServiceBlockingStub blockingStub;
+
+    @BeforeAll
+    public void setup() {
+        ManagedChannel managedChannel = ManagedChannelBuilder.forAddress("localhost", 6565)
+                .usePlaintext()
+                .build();
+
+        this.blockingStub = BankServiceGrpc.newBlockingStub(managedChannel);
+    }
+
+    @Test
+    public void balanceTest() {
+        BalanceCheckRequest balanceCheckRequest = BalanceCheckRequest.newBuilder()
+                .setAccountNumber(5)
+                .build();
+        Balance balance = this.blockingStub.getBalance(balanceCheckRequest);
+
+        System.out.println("Received: " + balance.getAmount());
+    }
+}
